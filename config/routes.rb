df7f3d0e2fae2d60293c851fa6ae883f6df66c90
github username <github email address>
2018-03-30 08:08:32 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
 end
